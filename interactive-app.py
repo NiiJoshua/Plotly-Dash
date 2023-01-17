c2236df1aa6c5a7cf84f16e6dc1544b9be2d0a14
NiJoshua <joshkobeng@gmail.com>
2023-01-17 15:38:17 +0000

    update

--- interactive-app.py
+++ interactive-app.py
@@ -1,3 +1,23 @@
-from dash import Dash, dcc
+from dash import Dash, dcc, Output, Input
 import dash_bootstrap_components as dbc
 
+# Build components
+app = Dash(__name__, external_stylesheets=[dbc.themes.SOLAR])
+mytext = dcc.Markdown(children = '')
+myinput = dbc.Input(value='# Hello World - First interactive app')
+
+# Layout
+app.layout = dbc.Container([mytext, myinput])
+
+# Callback
+@app.callback(
+    Output(mytext, component_property='children'),
+    Input(myinput, component_property='value')
+)
+
+def update_title(user_input): # arguements come from the component property of the in Input
+    return user_input # returned objects are assigned to the component property of the Output
+
+# Run app
+if __name__ == '__main__':
+    app.run_server(port=8052)
