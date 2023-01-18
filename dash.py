ae83446bf3394775fb6f18a849156d75de343574
NiJoshua <joshkobeng@gmail.com>
2023-01-18 10:41:43 +0000

    update

--- dash.py
+++ dash.py
@@ -5,3 +5,11 @@ import dash_bootstrap_components as dcc
 import plotly.express as px
 
 # data source
+data = "https://raw.githubusercontent.com/Coding-with-Adam/Dash-by-Plotly/master/Good_to_Know/Dash2.0/social_capital.csv"
+df = pd.read_csv(data)
+
+# Build Components
+app = Dash(__name__, external_stylesheets=[dbc.themes.LUX])
+mytitle = dcc.Markdown(Children='')
+mygraph = dcc.Graph(figure={})
+dropdonw = dcc.Dropdown(options)
