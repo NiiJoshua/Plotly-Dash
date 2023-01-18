e527a895788adf8a90805a44f2515b86ad88c7ad
NiJoshua <joshkobeng@gmail.com>
2023-01-18 09:03:47 +0000

    update

--- app-with-graph.py
+++ app-with-graph.py
@@ -1,14 +1,14 @@
 from dash import Dash, dcc, Output, Input
-import dash_bootstrap_componenets as dbc
+import dash_bootstrap_components as dbc
 import plotly.express as px
 
 # Add data
 df = px.data.medals_long()
 
 # Build components
-app = Dash(__name__, external_stysheets=[dbc.themes.VAPOR])
+app = Dash(__name__, external_stylesheets=[dbc.themes.VAPOR])
 mytitle = dcc.Markdown(children='# App that analyzes Olympic medals')
-mygraph = dcc.Graph(figures={})
+mygraph = dcc.Graph(figure={})
 dropdown = dcc.Dropdown(options = ['Bar Plot', 'Scatter Plot'],
                         value = 'Bar Plot', # displated when page loads
                         clearable=False
@@ -24,4 +24,12 @@ app.layout = dbc.Container([mytitle, mygraph, dropdown])
 )
 def update_graph(user_input):
     if user_input == 'Bar Plot':
-        
+        fig = px.bar(data_frame=df,x="nation", y="count",color="medal")
+    elif user_input == 'Scatter Plot':
+        fig = px.scatter(data_frame=df, x="count", y="nation", color="metal", symbol="metal")
+
+    return fig
+
+# run app
+if __name__ == '__main__':
+    app.run_server(port=8051)
